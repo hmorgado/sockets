182de7439aabebfa149df9218bd4b7789e34d7b4
heitor <heitorm.morgado@gmail.com>
2019-01-15 23:50:26 -0200

    bit a bit

--- client.py
+++ client.py
@@ -13,11 +13,13 @@ class mysocket:
 		self.sock.connect((kwargs['host'], kwargs['port']))
 
 	def send_it(self, msg):
-		total_sent = 0
+		total_sent = 1
 		msg_len = len(msg)
 		while total_sent < msg_len:
-			encoded_bytes_msg = str.encode(msg[total_sent:])
+			 							   # fun
+			encoded_bytes_msg = str.encode(msg[0:total_sent])
 			sent = self.sock.send(encoded_bytes_msg)
+			print('sent byte num:', total_sent)
 			if sent == 0:
 				raise RunTimeError("socket conn broken")
 			total_sent = total_sent + sent
@@ -26,7 +28,7 @@ class mysocket:
 def main():
 	sender = mysocket()
 	sender.connect(host='127.0.0.1', port=8080)
-	sender.send_it('oi bb\n')
+	sender.send_it('a b c d e f g h i j k l m n o p q r s \n')
 
 if __name__ == '__main__':
 	main()
